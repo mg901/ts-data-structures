bc96b9c070e6dc72d416e321e2e150b709cd5a2a
Maxim Alyoshin <mgreen097@gmail.com>
2023-11-24 20:02:04 +0400

    test(doubly-linked-list): add tests

--- src/doubly-linked-list/doubly-linked-list-node.ts
+++ src/doubly-linked-list/doubly-linked-list-node.ts
@@ -1,24 +1,17 @@
 export type Callback<T> = (value: T) => string;
 
-interface IDoublyLinkedListNode<T = any> {
-  value: T;
-  prev: IDoublyLinkedListNode<T> | null;
-  next: IDoublyLinkedListNode<T> | null;
-  toString(callback?: Callback<T>): string;
-}
-
-export class DoublyLinkedListNode<T = any> implements IDoublyLinkedListNode<T> {
+export class DoublyLinkedListNode<T = any> {
   constructor(
     public value: T,
-    public next: IDoublyLinkedListNode<T> | null = null,
-    public prev: IDoublyLinkedListNode<T> | null = null,
+    public next: DoublyLinkedListNode<T> | null = null,
+    public prev: DoublyLinkedListNode<T> | null = null,
   ) {
     this.value = value;
     this.next = next;
     this.prev = prev;
   }
 
-  toString(callback?: Callback<T>) {
+  toString(callback?: Callback<T>): string {
     return callback ? callback(this.value) : `${this.value}`;
   }
 }
