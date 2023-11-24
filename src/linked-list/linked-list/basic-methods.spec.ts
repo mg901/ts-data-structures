f4ea37b4a6fb59781945c99214782421b82347de
Maxim Alyoshin <mgreen097@gmail.com>
2023-11-24 15:46:48 +0400

    test: add test for `indexOf`, `find` and `fromArray` methods

--- src/linked-list/linked-list/basic-methods.spec.ts
+++ src/linked-list/linked-list/basic-methods.spec.ts
@@ -427,4 +427,132 @@ describe('LinkedList', () => {
       });
     });
   });
+
+  describe('indexOf', () => {
+    // Arrange
+    beforeEach(() => {
+      // Act
+      linkedList.append(1).append(2).append(3);
+    });
+
+    it('returns 0 if the index is at the head of the list', () => {
+      // Act and Assert
+      expect(linkedList.indexOf(1)).toBe(0);
+    });
+
+    it('returns the index of the given element if it exists in the list', () => {
+      // Act and Assert
+      expect(linkedList.indexOf(2)).toBe(1);
+    });
+
+    it("returns `-1` if the given element doesn't exist in the list", () => {
+      // Act and Assert
+      expect(linkedList.indexOf(4)).toBe(-1);
+    });
+
+    it('returns the index of the first occurrence of the given element', () => {
+      // Arrange
+      linkedList.append(2);
+
+      // Act and Assert
+      expect(linkedList.indexOf(2)).toBe(1);
+    });
+
+    it('returns `0` if the index is at the head of the list', () => {
+      // Act and Assert
+      expect(linkedList.indexOf(1)).toBe(0);
+    });
+
+    it('returns the index of the last element if the element is at the tail of the list', () => {
+      // Act and Assert
+      expect(linkedList.indexOf(3)).toBe(2);
+    });
+
+    describe('on an empty list', () => {
+      it('returns `-1` for any element', () => {
+        // Arrange
+        const emptyList = new LinkedList<number>();
+
+        // Act and Assert
+        expect(emptyList.indexOf(1)).toBe(-1);
+        expect(emptyList.indexOf(2)).toBe(-1);
+        expect(emptyList.indexOf(3)).toBe(-1);
+      });
+    });
+  });
+
+  describe('fromArray', () => {
+    it('creates an empty list when an empty array is passed', () => {
+      // Act
+      linkedList.fromArray([]);
+
+      // Act and Assert
+      expect(linkedList.isEmpty).toBeTruthy();
+    });
+
+    it('creates a list with the same nodes as the input array', () => {
+      // Act
+      linkedList.fromArray([1, 2, 3, 4]);
+
+      // Act and Assert
+      expect(linkedList.toString()).toBe('1,2,3,4');
+    });
+  });
+
+  describe('find', () => {
+    it('returns null for an empty list', () => {
+      // Act and Assert
+      expect(linkedList.find({ value: 1 })).toBeNull();
+    });
+
+    it('finds a node by value', () => {
+      // Arrange
+      linkedList.append(1).append(2);
+      const foundedNode = linkedList.find({ value: 2 });
+
+      // Act and Assert
+      expect(foundedNode?.toString()).toBe('2');
+    });
+
+    it('finds a node by predicate', () => {
+      // Arrange
+      linkedList.append(1).append(2).append(3);
+
+      const foundedNode = linkedList.find({
+        predicate: (value) => value > 2,
+      });
+
+      // Act and Assert
+      expect(foundedNode?.toString()).toBe('3');
+    });
+
+    it('returns null if a node is not found by value or predicate', () => {
+      // Act and Assert
+      expect(linkedList.find({ value: 3 })).toBeNull();
+    });
+
+    it('prioritizes predicate over value', () => {
+      // Arrange
+      linkedList.append(1).append(2);
+
+      const foundedNode = linkedList.find({
+        predicate: (value) => value > 1,
+      });
+
+      // Act and Assert
+      expect(foundedNode?.toString()).toBe('2');
+    });
+
+    it('returns the first node if multiple nodes match the predicate', () => {
+      // Arrange
+      linkedList.append(1).append(2).append(3).append(4);
+
+      const foundedNode = linkedList.find({
+        predicate: (value) => value > 1,
+      });
+
+      // Act and Assert
+      expect(foundedNode?.toString()).toBe('2');
+    });
+  });
 });
