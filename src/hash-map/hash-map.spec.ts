6f768b1389b70337685458a88266e2f6c6d8f890
Maxim Alyoshin <mgreen097@gmail.com>
2023-12-24 10:43:47 +0400

    feat: 🎸 add `hashMap`

---
+++ src/hash-map/hash-map.spec.ts
@@ -0,0 +1,30 @@
+import { describe, beforeEach, it, expect } from 'vitest';
+import { HashMap } from './hash-map';
+
+describe('HashMap', () => {
+  let hashMap: HashMap<string, number>;
+
+  // Arrange;
+  beforeEach(() => {
+    hashMap = new HashMap<string, number>();
+  });
+
+  it('set and get values', () => {
+    // Act
+    hashMap.set('one', 1);
+    hashMap.set('two', 2);
+
+    // Assert
+    expect(hashMap.get('one')).toBe(1);
+    expect(hashMap.get('two')).toBe(2);
+  });
+
+  it('updates existing values', () => {
+    // Act
+    hashMap.set('value', 1);
+    hashMap.set('value', 2);
+
+    // Assert
+    expect(hashMap.get('value')).toBe(2);
+  });
+});
