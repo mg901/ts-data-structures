fc097480a47e98614afc5e7408c6a7e651d22265
Maxim Alyoshin <mgreen097@gmail.com>
2024-02-25 02:28:06 +0400

    test(lru-cache-on-map): update tests

--- src/data-structures/cache/lru-cache-on-map/index.spec.ts
+++ src/data-structures/cache/lru-cache-on-map/index.spec.ts
@@ -6,10 +6,87 @@ describe('LRUCacheOnMap', () => {
 
   // Arrange
   beforeEach(() => {
-    cache = new LRUCache<string, number>(2);
+    cache = new LRUCache<string, number>(3);
+  });
 
-    cache.put('one', 1);
-    cache.put('two', 2);
+  describe('put', () => {
+    it('adds item correctly', () => {
+      // Act
+      cache.put('one', 1);
+
+      // Assert
+      expect(cache.toArray).toEqual([1]);
+      expect(cache.size).toBe(1);
+    });
+
+    it('fills the cache', () => {
+      // Act
+      cache.put('one', 1);
+      cache.put('two', 2);
+
+      // Assert
+      expect(cache.toArray).toEqual([1, 2]);
+      expect(cache.size).toBe(2);
+    });
+
+    it(`overwrites item's value correctly`, () => {
+      // Arrange
+      cache.put('key', 1);
+
+      // Act
+      cache.put('key', 2);
+
+      // Assert
+      expect(cache.toArray()).toEqual([2]);
+      expect(cache.size).toBe(1);
+    });
+
+    it('returns the correct size when exceeding capacity', () => {
+      // Arrange
+      cache.put('one', 1);
+      cache.put('two', 2);
+      cache.put('three', 3);
+      // 2, 3, 4
+
+      // Act
+      cache.put('four', 4);
+
+      // Assert
+      expect(cache.toArray()).toEqual([2, 3, 4]);
+      expect(cache.size).toBe(3);
+    });
+  });
+
+  describe('get', () => {
+    it('returns null for a non-existing item', () => {
+      // Act and Assert
+      expect(cache.get('one')).toBeNull();
+    });
+
+    it(`returns item's value correctly`, () => {
+      // Arrange
+      cache.put('one', 1);
+
+      // Act and Assert
+      expect(cache.toArray()).toEqual([1]);
+      expect(cache.size).toBe(1);
+    });
+
+    it('evicts the least recently used item when exceeding capacity', () => {
+      // Arrange
+      cache.put('one', 1);
+      cache.put('two', 2);
+      cache.put('three', 3);
+      cache.get('one');
+
+      // Act
+      cache.put('four', 4); // Should evict 2
+
+      // Assert
+      expect(cache.get('two')).toBeNull();
+      expect(cache.toArray()).toEqual([3, 1, 4]);
+      expect(cache.size).toBe(3);
+    });
   });
 
   describe('toStringTag', () => {
@@ -19,34 +96,5 @@ describe('LRUCacheOnMap', () => {
         '[object LRUCache]',
       );
     });
-  });
-
-  it('stores and retrieve values', () => {
-    // Assert
-    expect(cache.get('one')).toBe(1);
-    expect(cache.get('two')).toBe(2);
-  });
-
-  it('evicts least recently used item when exceeding capacity', () => {
-    // Act
-    cache.get('one'); // Move 1 to the front
-    cache.put('three', 3); // Should evict 2
-
-    // Assert
-    expect(cache.get('one')).toBe(1);
-    expect(cache.get('two')).toBe(-1);
-    expect(cache.get('three')).toBe(3);
-  });
-
-  it('updates value for an existing key', () => {
-    // Arrange
-    cache.put('value', 10);
-    expect(cache.get('value')).toBe(10);
-
-    // Act
-    cache.put('value', 20);
-
-    // Assert
-    expect(cache.get('value')).toBe(20);
   });
 });
--- src/data-structures/cache/lru-cache-on-map/index.ts
+++ src/data-structures/cache/lru-cache-on-map/index.ts
@@ -1,4 +1,11 @@
-export class LRUCache<Key = any, Value = any> {
+interface ILRUCache<Key, Value> {
+  get size(): number;
+  get(key: Key): Value | null;
+  put(key: Key, value: Value): this;
+  toArray(): Value[];
+}
+
+export class LRUCache<Key = any, Value = any> implements ILRUCache<Key, Value> {
   #capacity: number;
 
   #cache = new Map<Key, Value>();
@@ -7,17 +14,15 @@ export class LRUCache<Key = any, Value = any> {
     this.#capacity = capacity;
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  get [Symbol.toStringTag]() {
-    return 'LRUCache';
+  get size() {
+    return this.#cache.size;
   }
 
-  get(key: Key): Value | -1 {
-    if (!this.#cache.has(key)) return -1;
+  get(key: Key): Value | null {
+    if (!this.#cache.has(key)) return null;
 
     const value = this.#cache.get(key)!;
-    this.#cache.delete(key);
-    this.#cache.set(key, value);
+    this.#updateAccessOrder(key, value);
 
     return value;
   }
@@ -28,10 +33,30 @@ export class LRUCache<Key = any, Value = any> {
     }
 
     if (this.#capacity === this.#cache.size) {
-      const firstKey = this.#cache.keys().next().value;
-      this.#cache.delete(firstKey);
+      this.#evictLastRecentlyUsed();
     }
 
     this.#cache.set(key, value);
+
+    return this;
+  }
+
+  toArray() {
+    return Array.from(this.#cache, ([, value]) => value);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  get [Symbol.toStringTag]() {
+    return 'LRUCache';
+  }
+
+  #updateAccessOrder(key: Key, value: Value): void {
+    this.#cache.delete(key);
+    this.#cache.set(key, value);
+  }
+
+  #evictLastRecentlyUsed(): void {
+    const firstKey = this.#cache.keys().next().value;
+    this.#cache.delete(firstKey);
   }
 }
