aceac9aa3d594cdcacb598da3ec3b40fb4983e6c
Maxim Alyoshin <mgreen097@gmail.com>
2024-02-26 21:50:12 +0400

    test(lfu): it overwrites the value of the key

--- src/data-structures/cache/lfu/index.spec.ts
+++ src/data-structures/cache/lfu/index.spec.ts
@@ -11,6 +11,7 @@ describe('LFUCache', () => {
 
   it('returns initial state correctly', () => {
     expect(cache.size).toBe(0);
+    expect(cache.toArray()).toBe([]);
   });
 
   describe('put', () => {
@@ -19,20 +20,33 @@ describe('LFUCache', () => {
       cache.put('one', 1);
 
       // Assert
+      expect(cache.toArray()).toEqual([1]);
       expect(cache.size).toBe(1);
     });
-  });
 
-  it(`doesn't increase size on the cache overflow`, () => {
-    // Arrange
-    cache.put('one', 1);
-    cache.put('two', 2);
-    cache.put('three', 3);
+    it(`doesn't increase size on the cache overflow`, () => {
+      // Arrange
+      cache.put('one', 1);
+      cache.put('two', 2);
+      cache.put('three', 3);
 
-    // Act
-    cache.put('four', 4);
+      // Act
+      cache.put('four', 4);
 
-    // Assert
-    expect(cache.size).toBe(3);
+      // Assert
+      expect(cache.size).toBe(3);
+    });
+
+    it('overwrites the value of the key', () => {
+      // Arrange
+      cache.put('value', 1);
+
+      // Act
+      cache.put('value', 2);
+
+      // Arrange
+      expect(cache.toArray()).toEqual([2]);
+      expect(cache.size).toBe(1);
+    });
   });
 });
--- src/data-structures/cache/lfu/index.ts
+++ src/data-structures/cache/lfu/index.ts
@@ -2,16 +2,16 @@ import { DoublyLinkedList } from '@/data-structures/linked-lists/doubly-linked-l
 import { DoublyLinkedListNode } from '@/data-structures/linked-lists/doubly-linked-list/node';
 import type { Payload } from '../types';
 
-type KeyType = string | number | symbol;
-
-export class LFUCache<Key extends KeyType, Value> {
-  #keyFrequencyMap = {} as Record<Key, number>;
-
-  #buckets = {} as Record<number, DoublyLinkedList<Payload<Key, Value>>>;
-
-  #keyNodeMap = {} as Record<Key, DoublyLinkedListNode<Payload<Key, Value>>>;
-
+export class LFUCache<Key, Value> {
   #capacity: number;
+
+  #keyFrequencyMap = new Map<Key, number>();
+
+  #buckets = new Map<number, DoublyLinkedList<Payload<Key, Value>>>();
+
+  #keyNodeMap = new Map<Key, DoublyLinkedListNode<Payload<Key, Value>>>();
+
+  #minFrequency = 1;
 
   #size = 0;
 
@@ -23,18 +23,88 @@ export class LFUCache<Key extends KeyType, Value> {
     return this.#size;
   }
 
+  toArray(): Value[] {
+    const lists = this.#buckets.values();
+
+    return Array.from(lists)
+      .flatMap((list) => list?.toArray())
+      .map((pair) => pair.value);
+  }
+
   put(key: Key, value: Value) {
+    if (this.#keyNodeMap.has(key)) {
+      this.#deleteItemByKey(key);
+    }
+
     if (this.#size === this.#capacity) {
-      this.#size -= 1;
+      this.#evictLastFrequentlyUsedItem();
     }
 
     this.#addItem(key, value);
 
+    // console.log('minFrequency', this.#minFrequency);
+    // console.log('keyFreqMap', this.#keyFrequencyMap);
+    // console.log('buckets', this.#buckets);
+
     return this;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  #deleteItemByKey(key: Key) {
+    this.#deleteNodeByKey(key);
+    this.#updateFrequencyByKey(key);
+    this.#updateMinFrequencyByKey(key);
+    this.#size -= 1;
+  }
+
+  #deleteNodeByKey(key: Key) {
+    const keyNodeMap = this.#keyNodeMap;
+
+    const frequency = this.#keyFrequencyMap.get(key)!;
+    const nodeList = this.#buckets.get(frequency)!;
+    nodeList?.deleteByRef(keyNodeMap.get(key)!);
+
+    if (nodeList?.isEmpty) {
+      this.#buckets.delete(frequency);
+    }
+
+    keyNodeMap.delete(key);
+  }
+
+  #updateFrequencyByKey(key: Key) {
+    const prevFrequency = this.#keyFrequencyMap.get(key)!;
+    this.#keyFrequencyMap.set(key, prevFrequency + 1);
+  }
+
+  #updateMinFrequencyByKey(key: Key) {
+    this.#minFrequency = this.#keyFrequencyMap.get(key)!;
+  }
+
+  #evictLastFrequentlyUsedItem() {
+    this.#size -= 1;
+  }
+
   #addItem(key: Key, value: Value) {
+    let frequency = this.#getFrequencyByKey(key);
+    this.#keyFrequencyMap.set(key, frequency);
+
+    const nodesList = this.#getNodeListByFrequency(frequency);
+
+    nodesList.append({ key, value });
+    this.#buckets.set(frequency, nodesList);
+
+    const newNode = nodesList.tail!;
+    this.#keyNodeMap.set(key, newNode);
+
     this.#size += 1;
+  }
+
+  #getFrequencyByKey(key: Key) {
+    const INITIAL_FREQUENCY = 1;
+
+    return this.#keyFrequencyMap.get(key) ?? INITIAL_FREQUENCY;
+  }
+
+  #getNodeListByFrequency(freq: number) {
+    return this.#buckets.get(freq) ?? new DoublyLinkedList();
   }
 }
