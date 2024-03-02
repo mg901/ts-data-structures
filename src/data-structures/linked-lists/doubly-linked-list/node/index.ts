1e4ee1cc58e3cefc4cc1bccf8523a661b30cbcb6
mg901 <mgreen097@gmail.com>
2024-03-02 18:48:58 +0400

    fix(doubly-linked-list): add generic type for the data prop (#110)

--- src/data-structures/linked-lists/doubly-linked-list/node/index.ts
+++ src/data-structures/linked-lists/doubly-linked-list/node/index.ts
@@ -2,14 +2,14 @@ import { Node } from '@/shared/node';
 import { Nullable } from '@/shared/types';
 
 export class DoublyLinkedListNode<T = any> extends Node<T> {
-  next: Nullable<DoublyLinkedListNode>;
+  next: Nullable<DoublyLinkedListNode<T>>;
 
-  prev: Nullable<DoublyLinkedListNode>;
+  prev: Nullable<DoublyLinkedListNode<T>>;
 
   constructor(
     data: T,
-    next: Nullable<DoublyLinkedListNode> = null,
-    prev: Nullable<DoublyLinkedListNode> = null,
+    next: Nullable<DoublyLinkedListNode<T>> = null,
+    prev: Nullable<DoublyLinkedListNode<T>> = null,
   ) {
     super(data);
     this.next = next;
