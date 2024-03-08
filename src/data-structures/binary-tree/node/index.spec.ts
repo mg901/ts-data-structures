043bec074666404692fde9d291f91aeab4b42189
Maxim Alyoshin <mgreen097@gmail.com>
2024-03-08 23:02:28 +0400

    feat: add the `setLeft` and `setRight` methods

--- src/data-structures/binary-tree/node/index.spec.ts
+++ src/data-structures/binary-tree/node/index.spec.ts
@@ -1,28 +1,81 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { BinaryTreeNode } from './index';
 
 describe('BinaryTreeNode', () => {
+  let node1: BinaryTreeNode<number>;
+  let node2: BinaryTreeNode<number>;
+  let node3: BinaryTreeNode<number>;
+
+  // Arrange
+  beforeEach(() => {
+    node1 = new BinaryTreeNode(1);
+    node2 = new BinaryTreeNode(2);
+    node3 = new BinaryTreeNode(3);
+  });
+
   it('returns initial state correctly', () => {
     // Arrange
     const node = new BinaryTreeNode(1);
 
     // Act and Assert
     expect(node).toBeDefined();
+
     expect(node.data).toBe(1);
+    expect(node.parent).toBeNull();
     expect(node.left).toBeNull();
     expect(node.right).toBeNull();
   });
 
-  it('links nodes together', () => {
-    // Arrange
-    const leftChild = new BinaryTreeNode(2);
-    const rightChild = new BinaryTreeNode(3);
+  describe('setLeft', () => {
+    it('sets left child node correctly', () => {
+      // Act
+      node1.setLeft(node2);
 
-    const root = new BinaryTreeNode(1, leftChild, rightChild);
+      // Assert
+      expect(node1.left).toEqual(node2);
+      expect(node2.parent).toEqual(node1);
+    });
 
-    // Assert
-    expect(root.data).toBe(1);
-    expect(root.left?.data).toEqual(leftChild.data);
-    expect(root.right?.data).toEqual(rightChild.data);
+    it('disconnects the existing left child node before setting a new one', () => {
+      // Arrange
+      const oldLeft = node3;
+      node1.setLeft(oldLeft);
+
+      // Act
+      const newLeft = new BinaryTreeNode(4);
+      node1.setLeft(newLeft);
+
+      // Assert
+      expect(node1.left).toEqual(newLeft);
+      expect(newLeft.parent).toEqual(node1);
+      expect(oldLeft.parent).toBeNull();
+    });
+  });
+
+  describe('setRight', () => {
+    it('sets right child node correctly', () => {
+      // Act
+      node1.setRight(node2);
+
+      // Assert
+
+      expect(node1.right).toEqual(node2);
+      expect(node2.parent).toEqual(node1);
+    });
+
+    it('disconnects existing right child node before setting new one', () => {
+      // Arrange
+      const oldRight = node3;
+      node1.setRight(oldRight);
+
+      // Act
+      const newRight = new BinaryTreeNode(4);
+      node1.setRight(newRight);
+
+      // Assert
+      expect(node1.right).toEqual(newRight);
+      expect(newRight.parent).toEqual(node1);
+      expect(oldRight.parent).toBeNull();
+    });
   });
 });
--- src/data-structures/binary-tree/node/index.ts
+++ src/data-structures/binary-tree/node/index.ts
@@ -2,17 +2,37 @@ import { Node } from '@/shared/node';
 import { Nullable } from 'vitest';
 
 export class BinaryTreeNode<T = any> extends Node<T> {
+  parent: Nullable<BinaryTreeNode<T>> = null;
+
   left: Nullable<BinaryTreeNode<T>>;
 
   right: Nullable<BinaryTreeNode<T>>;
 
-  constructor(
-    data: T,
-    left: Nullable<BinaryTreeNode<T>> = null,
-    right: Nullable<BinaryTreeNode<T>> = null,
-  ) {
-    super(data);
-    this.left = left;
-    this.right = right;
+  setLeft(node: BinaryTreeNode<T>) {
+    if (this.left) {
+      this.left.parent = null;
+    }
+
+    this.left = node;
+
+    if (this.left) {
+      this.left.parent = this;
+    }
+
+    return this;
+  }
+
+  setRight(node: BinaryTreeNode<T>) {
+    if (this.right) {
+      this.right.parent = null;
+    }
+
+    this.right = node;
+
+    if (this.right) {
+      this.right.parent = this;
+    }
+
+    return this;
   }
 }
