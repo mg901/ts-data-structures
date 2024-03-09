9fce28959f1ee3615ef25ef575d6f11e74643c78
Maxim Alyoshin <mgreen097@gmail.com>
2024-03-10 01:26:00 +0400

    feat(binary-search-tree-node): add insert method

--- src/data-structures/tree/binary-search-tree/binary-search-tree-node/index.spec.ts
+++ src/data-structures/tree/binary-search-tree/binary-search-tree-node/index.spec.ts
@@ -8,5 +8,29 @@ describe('BinarySearchTreeNode', () => {
 
     // Assert
     expect(node).toBeDefined();
+    // Assert
+    expect(node.data).toBe(1);
+    expect(node.left).toBeNull();
+    expect(node.right).toBeNull();
+  });
+
+  describe('insert', () => {
+    it('inserts smaller values to the left', () => {
+      // Arrange
+      const root = new BinarySearchTreeNode(20);
+
+      // Act
+      const left = root.insert(3);
+      const right = root.insert(25);
+      const grandLeft = root.insert(2);
+      // node.insert(5);
+
+      // Assert
+      expect(left?.data).toBe(3);
+      expect(right?.data).toBe(25);
+      expect(grandLeft?.data).toBe(2);
+      // expect(node.left?.left?.data).toBe(2);
+      // expect(node.left?.right?.data).toBe(5);
+    });
   });
 });
--- src/data-structures/tree/binary-search-tree/binary-search-tree-node/index.ts
+++ src/data-structures/tree/binary-search-tree/binary-search-tree-node/index.ts
@@ -1,3 +1,41 @@
 import { BinaryTreeNode } from '@/data-structures/tree/binary-tree-node';
+import { Comparator, type CompareFunction } from '@/shared/comparator';
 
-export class BinarySearchTreeNode<T = any> extends BinaryTreeNode<T> {}
+export class BinarySearchTreeNode<T = any> extends BinaryTreeNode<T> {
+  left: BinarySearchTreeNode<T> | null = null;
+
+  right: BinarySearchTreeNode<T> | null = null;
+
+  #compare: Comparator<T>;
+
+  #compareFunction?: CompareFunction<T>;
+
+  constructor(data: T, compareFunction?: CompareFunction<T>) {
+    super(data);
+    this.#compareFunction = compareFunction;
+    this.#compare = new Comparator(compareFunction);
+  }
+
+  insert(value: T) {
+    const newNode = new BinarySearchTreeNode(value, this.#compareFunction);
+
+    if (this.#compare.lessThan(value, this.data)) {
+      if (this.left === null) {
+        this.setLeft(newNode);
+
+        return newNode;
+      }
+
+      this.left.insert(value);
+    } else {
+      if (this.right === null) {
+        this.setRight(newNode);
+
+        return newNode;
+      }
+      this.right.insert(value);
+    }
+
+    return this;
+  }
+}
