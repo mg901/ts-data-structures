4e067758bff77c39bce364b2ace1324ecaa2e055
Maxim Alyoshin <mgreen097@gmail.com>
2024-03-15 13:11:54 +0400

    refactor(promise): move the strategy object  to the top of the `then` method

--- src/data-structures/promise/index.ts
+++ src/data-structures/promise/index.ts
@@ -218,29 +218,7 @@ export class MyPromise<T = any> implements IMyPromise<T> {
     onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null,
   ) {
     return new MyPromise<TResult1 | TResult2>((resolve, reject) => {
-      type Handler = typeof onfulfilled | typeof onrejected;
-
-      const executeHandler = (handler: Handler) => {
-        try {
-          if (isFunction(handler)) {
-            const result = handler(this.#value as T);
-
-            if (isMyPromise(result)) {
-              result.then(resolve, reject);
-            } else {
-              resolve(result);
-            }
-          } else if (this.#state === STATE.FULFILLED) {
-            resolve(this.#value as TResult1 | TResult2);
-          } else {
-            reject(this.#value);
-          }
-        } catch (error) {
-          reject(error);
-        }
-      };
-
-      const handlers = {
+      const strategy = {
         [STATE.PENDING]: () => {
           this.#onfulfilledCallbacks.enqueue(() => executeHandler(onfulfilled));
           this.#onrejectedCallbacks.enqueue(() => executeHandler(onrejected));
@@ -249,7 +227,30 @@ export class MyPromise<T = any> implements IMyPromise<T> {
         [STATE.REJECTED]: () => executeHandler(onrejected),
       };
 
-      handlers[this.#state]();
+      strategy[this.#state]();
+
+      type Handler = typeof onfulfilled | typeof onrejected;
+
+      const self = this;
+      function executeHandler(handler: Handler) {
+        try {
+          if (isFunction(handler)) {
+            const result = handler(self.#value as T);
+
+            if (isMyPromise(result)) {
+              result.then(resolve, reject);
+            } else {
+              resolve(result);
+            }
+          } else if (self.#state === STATE.FULFILLED) {
+            resolve(self.#value as TResult1);
+          } else {
+            reject(self.#value);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      }
     });
   }
 
