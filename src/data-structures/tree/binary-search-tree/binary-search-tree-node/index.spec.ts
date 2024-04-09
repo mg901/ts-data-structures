47fa147016cd4881b2efa11588f2109a0b367a56
mg901 <mgreen097@gmail.com>
2024-04-09 16:51:46 +0400

    feat(binary-search-tree): add `BinarySearchTree` (#140)
    * chore: add `BinarySearchTreeNode`
    
    * feat(binary-search-tree-node): add insert method
    
    * build(dev-deps): bump dev-deps
    
    * test(binary-tree-node): fix test
    
    * feat: 🎸 add `find` method

---
+++ src/data-structures/tree/binary-search-tree/binary-search-tree-node/index.spec.ts
@@ -0,0 +1,66 @@
+import { describe, expect, it } from 'vitest';
+import { BinarySearchTreeNode } from './index';
+
+describe('BinarySearchTreeNode', () => {
+  it('returns initial state correctly', () => {
+    // Arrange
+    const node = new BinarySearchTreeNode(1);
+
+    // Assert
+    expect(node).toBeDefined();
+    // Assert
+    expect(node.data).toBe(1);
+    expect(node.left).toBeNull();
+    expect(node.right).toBeNull();
+  });
+
+  describe('insert', () => {
+    it('inserts smaller values to the left', () => {
+      // Arrange
+      const root = new BinarySearchTreeNode(20);
+
+      // Act
+      const left = root.insert(3);
+      const right = root.insert(25);
+      const grandLeft = left.insert(2);
+      // node.insert(5);
+
+      // Assert
+      expect(left?.data).toBe(3);
+      expect(right?.data).toBe(25);
+      expect(grandLeft?.data).toBe(2);
+    });
+  });
+
+  describe('find', () => {
+    it('finds node by the value correctly', () => {
+      // Arrange
+      const root = new BinarySearchTreeNode(2);
+      const leftNode = new BinarySearchTreeNode(1);
+      const rightNode = new BinarySearchTreeNode(4);
+
+      root.setLeft(leftNode);
+      root.setRight(rightNode);
+
+      // Act and Assert
+      expect(root.find(4)).toEqual(rightNode);
+      expect(root.find(10)).toBeNull();
+    });
+  });
+
+  describe('contains', () => {
+    it('checks if there is a node is the tree by value', () => {
+      // Arrange
+      const root = new BinarySearchTreeNode(4);
+      const leftNode = new BinarySearchTreeNode(2);
+      const rightNode = new BinarySearchTreeNode(6);
+
+      root.setLeft(leftNode);
+      root.setRight(rightNode);
+
+      // Act and Assert
+      expect(root.contains(2)).toBeTruthy();
+      expect(root.contains(10)).toBeFalsy();
+    });
+  });
+});
---
+++ src/data-structures/tree/binary-search-tree/binary-search-tree-node/index.ts
@@ -0,0 +1,62 @@
+import { BinaryTreeNode } from '@/data-structures/tree/binary-tree-node';
+import { Comparator, type CompareFunction } from '@/shared/comparator';
+import { Nullable } from '@/shared/types';
+
+export class BinarySearchTreeNode<T = any> extends BinaryTreeNode<T> {
+  left: Nullable<BinarySearchTreeNode<T>> = null;
+
+  right: Nullable<BinarySearchTreeNode<T>> = null;
+
+  #compare: Comparator<T>;
+
+  #compareFunction?: CompareFunction<T>;
+
+  constructor(data: T, compareFunction?: CompareFunction<T>) {
+    super(data);
+    this.#compareFunction = compareFunction;
+    this.#compare = new Comparator(compareFunction);
+  }
+
+  insert(value: T) {
+    const newNode = new BinarySearchTreeNode(value, this.#compareFunction);
+
+    if (this.#compare.lessThan(value, this.data)) {
+      if (this.left === null) {
+        this.setLeft(newNode);
+
+        return newNode;
+      }
+
+      this.left.insert(value);
+    } else {
+      if (this.right === null) {
+        this.setRight(newNode);
+
+        return newNode;
+      }
+      this.right.insert(value);
+    }
+
+    return this;
+  }
+
+  find(value: T): Nullable<BinarySearchTreeNode> {
+    if (this.#compare.equal(this.data, value)) {
+      return this;
+    }
+
+    if (this.#compare.lessThan(value, this.data) && this.left) {
+      return this.left.find(value);
+    }
+
+    if (this.#compare.greaterThan(value, this.data) && this.right) {
+      return this.right.find(value);
+    }
+
+    return null;
+  }
+
+  contains(value: T) {
+    return Boolean(this.find(value));
+  }
+}
