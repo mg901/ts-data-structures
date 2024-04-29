720212d44c7450d8f80cb05db0b0faa835df35e1
Maxim Alyoshin <mgreen097@gmail.com>
2024-04-29 17:23:51 +0400

    build(dev-deps): bump dev-deps

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  // @ts-ignore
   plugins: [tsconfigPaths()],
   test: {
     coverage: {
