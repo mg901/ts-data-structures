208371e016f3acb456d8347c188aec1feceee4f4
Maxim Alyoshin <mgreen097@gmail.com>
2024-12-15 23:41:18 +0300

    refactor: rewrite promise

--- src/data-structures/promise/index.ts
+++ src/data-structures/promise/index.ts
@@ -46,19 +46,24 @@ interface IMyPromise<T> {
   readonly [Symbol.toStringTag]: string;
 }
 
-const STATES = {
+const PROMISE_STATES = {
   PENDING: 'pending',
   FULFILLED: 'fulfilled',
   REJECTED: 'rejected',
 } as const;
 
+const PROMISE_REACTION_TYPES = {
+  FULFILL: 'fulfill',
+  REJECT: 'reject',
+} as const;
+
 interface PromiseFulfilledResult<T> {
-  status: typeof STATES.FULFILLED;
+  status: typeof PROMISE_STATES.FULFILLED;
   value: T;
 }
 
 interface PromiseRejectedResult {
-  status: typeof STATES.REJECTED;
+  status: typeof PROMISE_STATES.REJECTED;
   reason: any;
 }
 
@@ -66,21 +71,36 @@ type PromiseSettledResult<T> =
   | PromiseFulfilledResult<T>
   | PromiseRejectedResult;
 
-type QueueTask = {
-  fulfilled: () => void;
-  rejected: () => void;
+type PromiseCapability<T> = {
+  promise: IMyPromise<T>;
+  resolve: (value: T | PromiseLike<T>) => void;
+  reject: (reason?: any) => void;
+};
+
+type FulfillPromiseReaction<T = any> = {
+  capability: PromiseCapability<T>;
+  type: typeof PROMISE_REACTION_TYPES.FULFILL;
+  handler: ((value: T) => T | PromiseLike<T>) | null;
+};
+
+type RejectPromiseReaction<T = any> = {
+  capability: PromiseCapability<T>;
+  type: typeof PROMISE_REACTION_TYPES.REJECT;
+  handler: ((reason: any) => T | PromiseLike<T>) | null;
 };
 
 export class MyPromise<T = any> implements IMyPromise<T> {
-  #state: ValueOf<typeof STATES> = STATES.PENDING;
-
-  #completed = false;
+  #state: ValueOf<typeof PROMISE_STATES> = PROMISE_STATES.PENDING;
 
   #value: T | PromiseLike<T> | undefined = undefined;
 
-  #reason: any = undefined;
+  #fulfillReactions: FulfillPromiseReaction<T>[] | undefined = [];
 
-  #callbacksQueue: QueueTask[] = [];
+  #rejectReactions: RejectPromiseReaction<T>[] | undefined = [];
+
+  #alreadyResolved = false;
+
+  #isHandled = false;
 
   // --- Resolve --------------------
   static resolve(): MyPromise<void>;
@@ -101,9 +121,11 @@ export class MyPromise<T = any> implements IMyPromise<T> {
 
   // --- All --------------------
   static all<T>(values: Iterable<T | PromiseLike<T>> = []) {
-    return new MyPromise<Awaited<T>[]>((resolve, reject) => {
-      handleNonIterable(values);
+    if (!isIterable(values)) {
+      return MyPromise.reject(createNonIterableError(values));
+    }
 
+    return new MyPromise((resolve, reject) => {
       const items = Array.from(values);
       let unresolved = items.length;
 
@@ -125,7 +147,7 @@ export class MyPromise<T = any> implements IMyPromise<T> {
 
       function handleResolvedPromises() {
         if (unresolved === 0) {
-          resolve(items as Awaited<T>[]);
+          resolve(items);
         }
       }
     });
@@ -133,9 +155,11 @@ export class MyPromise<T = any> implements IMyPromise<T> {
 
   // --- Race --------------------
   static race<T>(values: Iterable<T | PromiseLike<T>> = []) {
-    return new MyPromise<Awaited<T>>((resolve, reject) => {
-      handleNonIterable(values);
+    if (!isIterable(values)) {
+      return MyPromise.reject(createNonIterableError(values));
+    }
 
+    return new MyPromise<Awaited<T>>((resolve, reject) => {
       for (const value of values) {
         MyPromise.resolve(value).then(resolve, reject);
       }
@@ -148,9 +172,11 @@ export class MyPromise<T = any> implements IMyPromise<T> {
   ): MyPromise<Awaited<T[number]>>;
   static any<T>(values: Iterable<T | PromiseLike<T>>): MyPromise<Awaited<T>>;
   static any<T>(values: Iterable<T | PromiseLike<T>> = []) {
-    return new MyPromise<Awaited<T>>((resolve, reject) => {
-      handleNonIterable(values);
+    if (!isIterable(values)) {
+      return MyPromise.reject(createNonIterableError(values));
+    }
 
+    return new MyPromise<Awaited<T>>((resolve, reject) => {
       const items = Array.from(values);
       let resolved = items.length;
       let errors: Error[] = new Array(items.length);
@@ -189,14 +215,18 @@ export class MyPromise<T = any> implements IMyPromise<T> {
     values: Iterable<T | PromiseLike<T>>,
   ): MyPromise<PromiseSettledResult<Awaited<T>>[]>;
   static allSettled<T>(values: Iterable<T | PromiseLike<T>> = []) {
+    if (!isIterable(values)) {
+      return MyPromise.reject(createNonIterableError(values));
+    }
+
     const items = Array.from(values).map((item) =>
       MyPromise.resolve(item).then(
         (value) => ({
-          status: STATES.FULFILLED,
+          status: PROMISE_STATES.FULFILLED,
           value,
         }),
         (reason) => ({
-          status: STATES.REJECTED,
+          status: PROMISE_STATES.REJECTED,
           reason,
         }),
       ),
@@ -207,19 +237,13 @@ export class MyPromise<T = any> implements IMyPromise<T> {
 
   // --- With Resolvers --------------------
   static withResolvers<T>() {
-    let resolve!: (value: T | PromiseLike<T>) => void;
-    let reject!: (reason?: any) => void;
+    return newPromiseCapability<T>(MyPromise);
+  }
 
-    const promise = new MyPromise<T>((res, rej) => {
-      resolve = res;
-      reject = rej;
-    });
-
-    return {
-      promise,
-      resolve,
-      reject,
-    };
+  // --- Species
+  // specification reference: https://tc39.es/ecma262/#sec-get-promise-%25symbol.species%25
+  static get [Symbol.species]() {
+    return this;
   }
 
   // --- Constructor --------------------
@@ -229,48 +253,99 @@ export class MyPromise<T = any> implements IMyPromise<T> {
       reject: (reason?: any) => void,
     ) => void,
   ) {
-    // Error Handling -----
     if (new.target === undefined) {
       throw new TypeError(
         `${this.constructor.name} constructor cannot be invoked without 'new'`,
       );
     }
 
-    if (!isFunction(executor)) {
+    if (!isCallable(executor)) {
       throw new TypeError(
-        `${this.constructor.name} resolver ${type(executor)} is not a function`,
+        `${this.constructor.name} resolver ${typeof executor} is not a function`,
       );
     }
 
-    // Resolve -----
-    const internalResolve = (value: T | PromiseLike<T>) => {
-      if (this.#completed) return;
-      this.#completed = true;
+    // https://tc39.es/ecma262/#sec-rejectpromise
+    const rejectPromise = (reason: any) => {
+      assertPromiseStatePending(this.#state !== PROMISE_STATES.PENDING);
 
-      this.#state = STATES.FULFILLED;
-      this.#value = value;
+      const reactions = this.#rejectReactions;
+      this.#value = reason;
+      this.#fulfillReactions = undefined;
+      this.#rejectReactions = undefined;
+      this.#state = PROMISE_STATES.REJECTED;
 
-      this.#executeCallbacksQueue();
+      triggerPromiseReactions(reactions!, reason);
     };
 
     // Reject -----
-    const internalReject = (reason: any) => {
-      if (this.#completed) return;
-      this.#completed = true;
+    // https://tc39.es/ecma262/#sec-promise-reject-functions
+    const reject = (reason: any) => {
+      if (this.#alreadyResolved) return;
+      this.#alreadyResolved = true;
 
-      this.#state = STATES.REJECTED;
-      this.#reason = reason;
-
-      this.#executeCallbacksQueue();
+      rejectPromise(reason);
     };
 
-    // Constructor execution -----
+    // https://tc39.es/ecma262/#sec-fulfillpromise
+    const fulfillPromise = (value: T | PromiseLike<T>) => {
+      assertPromiseStatePending(this.#state !== PROMISE_STATES.PENDING);
+
+      const reactions = this.#fulfillReactions;
+      this.#value = value;
+      this.#fulfillReactions = undefined;
+      this.#rejectReactions = undefined;
+      this.#state = PROMISE_STATES.FULFILLED;
+
+      triggerPromiseReactions(reactions!, value);
+    };
+
+    //  Resolve -----
+    // https://tc39.es/ecma262/#sec-promise-resolve-functions
+    const resolve = (resolution: T | PromiseLike<T>) => {
+      const promise = this;
+
+      if (this.#alreadyResolved) return;
+      this.#alreadyResolved = true;
+
+      if (resolution === promise) {
+        throw new TypeError(
+          `Chaining cycle detected for promise #<${promise.constructor.name}>`,
+        );
+      }
+
+      if (!isObject(resolution)) {
+        fulfillPromise(resolution);
+
+        return;
+      }
+
+      try {
+        const thenAction = (resolution as PromiseLike<T>).then;
+
+        if (!isCallable(thenAction)) {
+          fulfillPromise(resolution);
+
+          return;
+        }
+
+        // https://tc39.es/ecma262/#sec-newpromiseresolvethenablejob
+        hostEnqueuePromiseJob(() => {
+          try {
+            thenAction.call(resolution, fulfillPromise, rejectPromise);
+          } catch (error) {
+            rejectPromise(error);
+          }
+        });
+      } catch (error) {
+        rejectPromise(error);
+      }
+    };
+
     try {
-      executor(internalResolve, internalReject);
+      executor(resolve, reject);
     } catch (error) {
-      queueMicrotask(() => {
-        internalReject(error);
-      });
+      reject(error);
     }
   }
 
@@ -279,78 +354,36 @@ export class MyPromise<T = any> implements IMyPromise<T> {
     onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | null,
     onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null,
   ) {
-    return new MyPromise((resolve, reject) => {
-      if (this.#state === STATES.PENDING) {
-        this.#callbacksQueue.push({
-          fulfilled: () => handleFulfilled(this.#value as T),
-          rejected: () => handleRejected(this.#reason),
-        });
-      } else if (this.#state === STATES.FULFILLED) {
-        if (isThenable(this.#value)) {
-          this.#value.then(handleFulfilled, handleFulfilled);
-        } else {
-          handleFulfilled(this.#value as T);
-        }
-      } else {
-        handleRejected(this.#reason);
-      }
+    const promiseCapability = newPromiseCapability<TResult1 | TResult2>(
+      MyPromise,
+    );
+    const onFulfilledCallback = !isCallable(onfulfilled) ? null : onfulfilled;
+    const onRejectedCallback = !isCallable(onrejected) ? null : onrejected;
 
-      function handleFulfilled(value: T) {
-        if (isFunction(onfulfilled)) {
-          queueMicrotask(() => {
-            executeCallback(onfulfilled, value);
-          });
-        } else {
-          resolve(value);
-        }
-      }
+    const fulfillReaction: FulfillPromiseReaction = {
+      capability: promiseCapability,
+      type: PROMISE_REACTION_TYPES.FULFILL,
+      handler: onFulfilledCallback,
+    };
 
-      function handleRejected(reason: any) {
-        if (isFunction(onrejected)) {
-          queueMicrotask(() => {
-            executeCallback(onrejected, reason);
-          });
-        } else {
-          reject(reason);
-        }
-      }
+    const rejectReaction: RejectPromiseReaction = {
+      capability: promiseCapability,
+      type: PROMISE_REACTION_TYPES.REJECT,
+      handler: onRejectedCallback,
+    };
 
-      function executeCallback(
-        callback: typeof onfulfilled | typeof onrejected,
-        data: any,
-      ) {
-        try {
-          const result = callback && callback(data);
-
-          if (isThenable(result)) {
-            result.then(resolve, reject);
-          } else {
-            resolve(result);
-          }
-        } catch (error) {
-          reject(error);
-        }
-      }
-    });
-  }
-
-  #executeCallbacksQueue() {
-    let index = 0;
-    const chain = this.#callbacksQueue;
-
-    while (index !== chain.length) {
-      const { fulfilled, rejected } = chain[index];
-
-      if (this.#state === STATES.FULFILLED) {
-        fulfilled();
-      } else {
-        rejected();
-      }
-
-      index += 1;
+    if (this.#state === PROMISE_STATES.PENDING) {
+      this.#fulfillReactions!.push(fulfillReaction);
+      this.#rejectReactions!.push(rejectReaction);
+    } else if (this.#state === PROMISE_STATES.FULFILLED) {
+      promiseReactionJob(fulfillReaction, this.#value);
+    } else {
+      promiseReactionJob(rejectReaction, this.#value);
     }
 
-    this.#callbacksQueue = [];
+    this.#isHandled = true;
+
+    return promiseCapability.promise;
   }
 
   // --- Catch --------------------
@@ -364,14 +397,14 @@ export class MyPromise<T = any> implements IMyPromise<T> {
   finally(onfinally?: (() => void) | undefined | null) {
     return this.then(
       (value) => {
-        if (isFunction(onfinally)) {
+        if (isCallable(onfinally)) {
           onfinally();
         }
 
         return value;
       },
       (reason) => {
-        if (isFunction(onfinally)) {
+        if (isCallable(onfinally)) {
           onfinally();
         }
 
@@ -385,30 +418,101 @@ export class MyPromise<T = any> implements IMyPromise<T> {
   }
 }
 
-// --- Utils --------------------
-function handleNonIterable(it: any): void {
-  if (it[Symbol.iterator]) return;
+function triggerPromiseReactions(
+  reactions: (FulfillPromiseReaction | RejectPromiseReaction)[],
+  argument: any,
+) {
+  for (const reaction of reactions) {
+    promiseReactionJob(reaction, argument);
+  }
+}
 
+function promiseReactionJob(
+  reaction: FulfillPromiseReaction | RejectPromiseReaction,
+  argument: any,
+) {
+  return hostEnqueuePromiseJob(() => {
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    const { capability, type, handler } = reaction;
+    const { resolve, reject } = capability;
+
+    if (handler == null) {
+      if (type === PROMISE_REACTION_TYPES.FULFILL) {
+        resolve(argument);
+      } else {
+        reject(argument);
+      }
+
+      return;
+    }
+
+    try {
+      const result = handler(argument);
+
+      if (isThenable(result)) {
+        result.then(resolve, reject);
+      } else {
+        resolve(result);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+function hostEnqueuePromiseJob(job: () => void) {
+  queueMicrotask(job);
+}
+
+// --- Utils --------------------
+function assertPromiseStatePending(condition: boolean) {
+  if (condition) {
+    throw new TypeError('Promise state must be pending.');
+  }
+}
+
+function isIterable(it: any) {
+  return Boolean(it[Symbol.iterator]);
+}
+
+function createNonIterableError(it: any) {
   let prefix = `${type(it)}`;
 
   if (type(it) === 'number' || type(it) === 'boolean') {
     prefix += ` ${it}`;
   }
 
-  throw new TypeError(
+  // eslint-disable-next-line consistent-return
+  return TypeError(
     `${prefix} is not iterable (cannot read property Symbol(Symbol.iterator))`,
   );
 }
 
+function newPromiseCapability<T>(C: typeof MyPromise) {
+  let resolve!: (value: T | PromiseLike<T>) => void;
+  let reject!: (reason?: any) => void;
+
+  const promise = new C<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  return {
+    promise,
+    resolve,
+    reject,
+  };
+}
+
 function isThenable(it: any): it is PromiseLike<unknown> {
-  return Boolean(isObject(it) && isFunction(it.then));
+  return isObject(it) && isCallable(it.then);
 }
 
 function isObject(it: unknown) {
-  return type(it) === 'object' ? it !== null : isFunction(it);
+  return type(it) === 'object' ? it !== null : isCallable(it);
 }
 
-function isFunction(it: unknown): it is (...args: unknown[]) => unknown {
+function isCallable(it: unknown): it is (...args: any[]) => any {
   return type(it) === 'function';
 }
 
