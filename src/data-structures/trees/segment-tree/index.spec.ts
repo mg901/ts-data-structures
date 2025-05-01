16029a3ef9b5ce71d026a3f001241a1e766f4021
Maxim Alyoshin <mgreen097@gmail.com>
2025-05-01 20:29:37 +0300

    chore(segment-tree): add `updateRange` method

--- src/data-structures/trees/segment-tree/index.spec.ts
+++ src/data-structures/trees/segment-tree/index.spec.ts
@@ -41,8 +41,8 @@ describe('Segment Tree', () => {
       st.update(0, 10);
 
       // Assert
-      expect(tree[0]).toBe(19); // 10 + 2 + 3 + 4
-      expect(tree[1]).toBe(12); // 10 + 2
+      expect(tree[0]).toBe(10 + 2 + 3 + 4);
+      expect(tree[1]).toBe(10 + 2);
       expect(tree[3]).toBe(10);
     });
 
@@ -55,8 +55,8 @@ describe('Segment Tree', () => {
       st.update(3, 7);
 
       // Assert
-      expect(tree[0]).toBe(13); // 1 + 2 + 3 + 7
-      expect(tree[2]).toBe(10); // 3 + 7
+      expect(tree[0]).toBe(1 + 2 + 3 + 7);
+      expect(tree[2]).toBe(3 + 7);
       expect(tree[6]).toBe(7);
     });
 
@@ -70,11 +70,68 @@ describe('Segment Tree', () => {
       st.update(2, 2);
 
       // Assert
-      expect(tree[0]).toBe(13); // 5 + 1 + 2 + 5
-      expect(tree[1]).toBe(6); // 5 + 1
-      expect(tree[2]).toBe(7); // 2 + 7
+      expect(tree[0]).toBe(5 + 1 + 2 + 5);
+      expect(tree[1]).toBe(5 + 1);
+      expect(tree[2]).toBe(2 + 5);
       expect(tree[4]).toBe(1);
       expect(tree[5]).toBe(2);
+    });
+  });
+
+  describe('query', () => {
+    it('returns 0 for out-of-range query', () => {
+      // Arrange
+      const st = new SegmentTree([1, 2, 3, 4]);
+
+      // Act and Assert
+      expect(st.query(0, -1)).toBe(0);
+      expect(st.query(4, 3)).toBe(0);
+    });
+
+    it('returns sum for the range', () => {
+      // Arrange
+      const st = new SegmentTree([1, 2, 3, 4]);
+
+      // Act and Assert
+      expect(st.query(0, 3)).toBe(10);
+      expect(st.query(-5, 3)).toBe(10);
+      expect(st.query(0, 10)).toBe(10);
+    });
+
+    it('returns sum for subrange', () => {
+      // Arrange
+      const st = new SegmentTree([1, 2, 3, 4]);
+
+      // Act and Assert
+      expect(st.query(0, 0)).toBe(1);
+      expect(st.query(0, 1)).toBe(3);
+      expect(st.query(1, 2)).toBe(5);
+      expect(st.query(2, 3)).toBe(7);
+    });
+  });
+
+  describe('updateRange', () => {
+    it('performs range update', () => {
+      // Arrange
+      const st = new SegmentTree([1, 2]);
+      const tree = st.getTree();
+
+      // Act
+      st.updateRange(0, 1, 2);
+
+      // Assert
+      expect(tree[0]).toBe(7);
+      expect(tree[1]).toBe(1);
+      expect(tree[2]).toBe(2);
+
+      // Act
+      expect(st.query(0, 1)).toBe(7);
+      expect(tree[1]).toBe(1);
+      expect(tree[2]).toBe(2);
+
+      expect(st.query(0, 0)).toBe(3);
+      expect(tree[1]).toBe(3);
+      expect(tree[2]).toBe(4);
     });
   });
 });
--- src/data-structures/trees/segment-tree/index.ts
+++ src/data-structures/trees/segment-tree/index.ts
@@ -1,15 +1,18 @@
 export class SegmentTree {
-  #length: number;
+  #n: number;
 
   #tree: number[];
 
+  #lazy: number[];
+
   constructor(arr: number[]) {
-    this.#length = arr.length;
-    this.#tree = new Array(this.#length * 4);
-    this.build(arr, 0, 0, this.#length - 1);
+    this.#n = arr.length;
+    this.#tree = new Array(this.#n * 4);
+    this.#lazy = new Array(this.#n * 4).fill(0);
+    this.build(arr, 0, 0, this.#n - 1);
   }
 
-  build(arr: number[], node: number, left: number, right: number) {
+  build(arr: number[], node: number, left: number, right: number): void {
     if (left === right) {
       this.#tree[node] = arr[left];
 
@@ -28,8 +31,10 @@ export class SegmentTree {
     newValue: number,
     node = 0,
     left = 0,
-    right = this.#length - 1,
-  ) {
+    right = this.#n - 1,
+  ): void {
+    this.#propagate(node, left, right);
+
     if (left === right) {
       this.#tree[node] = newValue;
 
@@ -45,6 +50,67 @@ export class SegmentTree {
     }
 
     this.#tree[node] = this.#tree[node * 2 + 1] + this.#tree[node * 2 + 2];
+  }
+
+  updateRange(
+    start: number,
+    end: number,
+    delta = 0,
+    node = 0,
+    left = 0,
+    right = this.#n - 1,
+  ): void {
+    this.#propagate(node, left, right);
+
+    if (start > right || end < left) return;
+
+    if (start <= left && end >= right) {
+      this.#lazy[node] += delta;
+      this.#propagate(node, left, right);
+
+      return;
+    }
+
+    const mid = left + Math.floor((right - left) / 2);
+    this.updateRange(start, end, delta, node * 2 + 1, left, mid);
+    this.updateRange(start, end, delta, node * 2 + 2, mid + 1, right);
+
+    this.#tree[node] = this.#tree[node * 2 + 1] + this.#tree[node * 2 + 2];
+  }
+
+  #propagate(node: number, left: number, right: number) {
+    if (this.#lazy[node] === 0) return;
+
+    this.#tree[node] += (right - left + 1) * this.#lazy[node];
+
+    if (left !== right) {
+      this.#lazy[node * 2 + 1] += this.#lazy[node];
+      this.#lazy[node * 2 + 2] += this.#lazy[node];
+    }
+
+    this.#lazy[node] = 0;
+  }
+
+  query(
+    start: number,
+    end: number,
+    node = 0,
+    left = 0,
+    right = this.#n - 1,
+  ): number {
+    this.#propagate(node, left, right);
+
+    if (start > right || end < left) return 0;
+
+    if (start <= left && end >= right) {
+      return this.#tree[node];
+    }
+
+    const mid = left + Math.floor((right - left) / 2);
+    const leftSum = this.query(start, end, node * 2 + 1, left, mid);
+    const rightSum = this.query(start, end, node * 2 + 2, mid + 1, right);
+
+    return leftSum + rightSum;
   }
 
   getTree() {
