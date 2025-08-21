b31106ba0391a93a133b3230aac92f4606969116
Maxim Alyoshin <mgreen097@gmail.com>
2025-08-21 23:55:08 +0300

    test(disjoint-set): improve unit tests

--- src/data-structures/disjoint-set/index.spec.ts
+++ src/data-structures/disjoint-set/index.spec.ts
@@ -1,48 +1,60 @@
-import { describe, expect, test } from 'vitest';
+import { beforeEach, describe, expect, test } from 'vitest';
 import { DisjointSet } from './index.js';
 
 describe('DisjointSet', () => {
+  let ds: DisjointSet;
+
+  beforeEach(() => {
+    ds = new DisjointSet(5);
+  });
   test('initialize', () => {
-    const ds = new DisjointSet(5);
     for (let i = 0; i < 5; i += 1) {
+      // Act & Assert
       expect(ds.find(i)).toBe(i);
     }
   });
 
   test('union', () => {
-    const ds = new DisjointSet(5);
-    expect(ds.union(0, 1)).toBe(true);
-    expect(ds.connected(0, 1)).toBe(true);
-    expect(ds.connected(1, 0)).toBe(true);
+    // Act & Assert
+    expect(ds.union(0, 1)).toBeTruthy();
+    expect(ds.connected(0, 1)).toBeTruthy();
+    expect(ds.connected(1, 0)).toBeTruthy();
   });
 
   test('already connected elements', () => {
-    const ds = new DisjointSet(5);
+    // Act
     ds.union(0, 1);
-    expect(ds.union(0, 1)).toBe(false);
+
+    // Act & Asset
+    expect(ds.union(0, 1)).toBeFalsy();
   });
 
   test('multiple unions', () => {
-    const ds = new DisjointSet(5);
+    // Act
     ds.union(0, 1);
     ds.union(1, 2);
     ds.union(3, 4);
 
-    expect(ds.connected(0, 2)).toBe(true);
-    expect(ds.connected(0, 3)).toBe(false);
-    expect(ds.connected(3, 4)).toBe(true);
+    // Assert
+    expect(ds.connected(0, 2)).toBeTruthy();
+    expect(ds.connected(0, 3)).toBeFalsy();
+    expect(ds.connected(3, 4)).toBeTruthy();
 
+    // Act
     ds.union(2, 4);
-    expect(ds.connected(0, 4)).toBe(true);
+
+    // Assert
+    expect(ds.connected(0, 4)).toBeTruthy();
   });
 
   test('unrelated elements', () => {
-    const ds = new DisjointSet(5);
+    // Act
     ds.union(0, 1);
     ds.union(2, 3);
 
-    expect(ds.connected(0, 2)).toBe(false);
-    expect(ds.connected(1, 3)).toBe(false);
-    expect(ds.connected(4, 4)).toBe(true);
+    // Assert
+    expect(ds.connected(0, 2)).toBeFalsy();
+    expect(ds.connected(1, 3)).toBeFalsy();
+    expect(ds.connected(4, 4)).toBeTruthy();
   });
 });
--- src/data-structures/disjoint-set/index.ts
+++ src/data-structures/disjoint-set/index.ts
@@ -1,3 +1,5 @@
+// Explanation:
+
 export class DisjointSet {
   #parent;
 
